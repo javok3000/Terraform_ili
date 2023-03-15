2da421533ed636a3bea9cd8daf8ed47bedd132f8
Javier Sebastiani <jsebastiani@gestyde.com>
2023-03-15 14:35:39 +0100

    - EC2 creation with the following preferences:     * Instances type.     * Subnet.     * Security Group.     * Proper pair of ssh key.     * Costum ami     * Custum Name - Link a Elastic Ip to the EC2 mentioned before.

---
+++ variable.tf
@@ -0,0 +1,59 @@
+# variable "access_key" {
+#   description = "Access-key-for-AWS"
+#   default     = "No access key found"
+# }
+ 
+# variable "secret_key" {
+#   description = "Secret-key-for-AWS"
+#   default     = "No secret key found"
+# }
+
+variable "name" {
+  type        = string
+  default     = "ILITIA_API_<nombre_clinica>"
+  description = "EC2 Name"
+}
+
+variable "ami" {
+  type        = string
+  default     = "No ami found"
+  description = "AWS EC2 AMI id."
+
+  validation{
+    condition     = length(var.ami) > 4 && substr(var.ami,0,4) == "ami-"
+    error_message = "Enter a valid AMI id"
+  }
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "No instance type found"
+  description = "Instance EC2 type"
+  sensitive   = true
+}
+
+
+variable "group_id" {
+  type        = list
+  description = "Security group"
+}
+
+variable "ssh_key" {
+  type        = string
+  default     = "No key pair found"
+  description = "SSH key"
+  sensitive   = true
+}
+
+variable "region" {
+  type        = string
+  default     = "No region found"
+  description = "description"
+}
+
+variable subnet_ids {
+  type        = list
+  description = "List of subnet in default vpc"
+}
+
+
